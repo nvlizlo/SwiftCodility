7ef1223e657b41ac9be67cb3d86da302ba1827b3
nvlizlo <vlizlo93@gmail.com>
2017-10-06 01:02:11 +0300

    Added solution for PermCheck

--- Lesson 4 - Counting Elements/PermCheck.playground/Contents.swift
+++ Lesson 4 - Counting Elements/PermCheck.playground/Contents.swift
@@ -3,17 +3,25 @@
 import UIKit
 
 public func solution(_ A : inout [Int]) -> Int {
-    let count = A.count
-    let sum = count * (count + 1) / 2
-    
-    var arraySum = 0
-    
-    for i in 0..<A.count {
-        arraySum += A[i]
+    A.insert(0, at: 0)
+    var a = Array(repeating: 0, count: A.count)
+    a[0] = 1
+    for i in 1..<A.count {
+        if A[i] < a.count {
+            a[A[i]] += 1
+        } else {
+            return 0
+        }
     }
     
-    return sum == arraySum ? 1 : 0
+    for i in 1..<a.count {
+        if a[i] != 1 {
+            return 0
+        }
+    }
+    
+    return 1
 }
 
-var array = [4, 1, 2, 3]
+var array = [4, 1, 3, 2]
 solution(&array)
