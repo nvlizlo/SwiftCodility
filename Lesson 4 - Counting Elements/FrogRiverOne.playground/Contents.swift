4cc84dc4f6cdfc2bf5aed521c034ccff9f0fae86
nvlizlo <vlizlo93@gmail.com>
2018-02-16 14:09:18 +0200

    Added solution for Counting elemets-Frog river one

---
+++ Lesson 4 - Counting Elements/FrogRiverOne.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public func solution(_ X : Int, _ A : inout [Int]) -> Int {
+    
+    var occurs:[Int] = Array(repeating: -1, count: X + 1)
+
+    for i in 0..<A.count {
+        if occurs[A[i]] == -1 {
+            occurs[A[i]] = i
+        }
+    }
+    
+    occurs.removeFirst()
+    
+    if (occurs.filter { $0 == -1 }).count == 0 {
+        return occurs.max(by: { a, b in
+            a < b
+        }) ?? 0
+    }
+    
+    return -1
+}
+
+var a = [1, 3, 1, 4, 2, 3, 5, 4]
+
+solution(5, &a)
