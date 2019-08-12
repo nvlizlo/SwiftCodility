f2a1f1e5ac2f16ef24b5049a5bacf8a5556be4b0
nvlizlo <>
2019-08-13 01:11:43 +0300

    Added solution for Number of Disk Intersections

---
+++ Lesson 6 - Sorting/NumberOfDiskIntersections.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+public func solution(_ A : inout [Int]) -> Int {
+    
+    if A.isEmpty || A.count > 100_000 {
+        return 0
+    }
+    
+    let maxValue = A.max() ?? 0
+    let sortedArray = A.enumerated().map { ($0.offset - $0.element, $0.offset + $0.element) }.sorted { $0.0 < $1.0 }
+    
+    var intersections = 0
+    
+    for i in 0..<A.count {
+        
+        let iRight = sortedArray[i].1
+        let maxIndex = min(iRight + maxValue + 1, A.count)
+        
+        for j in i+1..<maxIndex {
+            
+            let jLeft = sortedArray[j].0
+            
+            if iRight >= jLeft {
+                intersections += 1
+            }
+        }
+        
+        if intersections > 10_000_000 {
+            return -1
+        }
+    }
+    
+    return intersections
+}
+
+var a = [1, 5, 2, 1, 4, 0] // 11
+solution(&a)
