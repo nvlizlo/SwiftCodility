6b811c0374bbb0fff95f7b2d6254fe9f62612805
Nazariy Vlizlo <nazariy.vlizlo@openbet.com>
2024-09-05 22:11:20 +0300

    Updated solution to CountingElements/MaxCounters
    gitignore

--- Lesson 4 - Counting Elements/MaxCounters.playground/Contents.swift
+++ Lesson 4 - Counting Elements/MaxCounters.playground/Contents.swift
@@ -3,32 +3,28 @@
 import UIKit
 
 public func solution(_ N : Int, _ A : inout [Int]) -> [Int] {
-    let maxCounter = N + 1
-    var counter = Array(repeating: 0, count: maxCounter)
+    var counter = Array(repeating: 0, count: N)
     var maxValue = 0
     var g = 0
-    
+
     for i in A {
         if i <= N {
-            if counter[i] < g {
-                counter[i] = g
+            if counter[i - 1] < g {
+                counter[i - 1] = g
             }
-            counter[i] += 1
-            print(counter)
-            maxValue = max(maxValue, counter[i])
+            counter[i - 1] += 1
+            maxValue = max(maxValue, counter[i - 1])
         } else {
             g = maxValue
         }
     }
-    
-    counter.remove(at: 0)
-    
+
     for i in 0..<counter.count {
         if counter[i] < g {
             counter[i] = g
         }
     }
-    
+
     return counter
 }
 
