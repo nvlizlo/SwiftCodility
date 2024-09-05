04c98c846b5913ff44bcc4cb2941c032ac5618a7
Nazariy Vlizlo <nazariy.vlizlo@openbet.com>
2024-09-05 19:14:30 +0300

    Updated solution to Arrays/CyclicRotation
    updated gitignore for that

--- Lesson 2 - Arrays/CyclicRotation.playground/Contents.swift
+++ Lesson 2 - Arrays/CyclicRotation.playground/Contents.swift
@@ -3,23 +3,19 @@
 import UIKit
 
 public func solution(_ A : inout [Int], _ K : Int) -> [Int] {
-    if !((1...100).contains(A.count) && (0...100).contains(K)) {
-        return []
-    }
-    
-    var array = Array(repeating: A[0], count: A.count)
-    
+    guard (1...100).contains(A.count), (0...100).contains(K) else { return [] }
+
+    var result = Array(repeating: 0, count: A.count)
+
     for i in 0..<A.count {
         let j = i + K
-        array[j % A.count] = A[i]
-        
+        result[j % A.count] = A[i]
     }
-    
-    return array
+    return result
 }
 
 var array = [3, 8, 9, 7, 6]
 var a = [1,2]
 var b = [1]
 var c = [Int]()
-solution(&c, 3)
+solution(&array, 3)
