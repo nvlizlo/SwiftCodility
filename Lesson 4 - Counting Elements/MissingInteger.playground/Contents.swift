2f019f423028cac2429e2bdea27cdff09f49fba9
Nazariy Vlizlo <nazariy.vlizlo@openbet.com>
2024-09-05 22:36:01 +0300

    Updated solution to CountingElements/MissingInteger
    Updated gitignore

--- Lesson 4 - Counting Elements/MissingInteger.playground/Contents.swift
+++ Lesson 4 - Counting Elements/MissingInteger.playground/Contents.swift
@@ -4,20 +4,19 @@ import UIKit
 
 public func solution(_ A : inout [Int]) -> Int {
     var occurs = Array(repeating: 0, count: A.count + 1)
-    
+
     for i in A {
-        if i > 0 && i < occurs.count {
+        if (1..<occurs.count).contains(i) {
             occurs[i] += 1
         }
     }
-    
+
     for i in 1..<occurs.count {
         if occurs[i] == 0 {
             return i
         }
     }
-    
-    return occurs.reduce(0, +) == A.count ? A.count + 1 : 1
+    return A.count + 1
 }
 
 var array = [1, 2, 3]
@@ -29,5 +28,3 @@ solution(&a)
 solution(&b)
 solution(&c)
 solution(&array)
-
-
