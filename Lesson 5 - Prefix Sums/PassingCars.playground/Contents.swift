cf064d8bab12600d1d4e02709e8f017454080259
Nazariy Vlizlo <nazariy.vlizlo@openbet.com>
2024-09-06 11:13:36 +0300

    Updated solution to PrefixSums/PassingCars
    And gitignore

--- Lesson 5 - Prefix Sums/PassingCars.playground/Contents.swift
+++ Lesson 5 - Prefix Sums/PassingCars.playground/Contents.swift
@@ -3,43 +3,38 @@
 import UIKit
 
 public func solution(_ A : inout [Int]) -> Int {
-    
     var counter = 0
     var result = 0
-    
+
     for i in 0..<A.count {
         if A[i] == 1 {
             result += counter
         } else {
             counter += 1
         }
-        
-        if result > 1_000_000_000 {
-            return -1
-        }
+
+        guard result <= 1_000_000_000 else { return -1 }
     }
-    
+
     return result
 }
 
 public func solution2(_ A : inout [Int]) -> Int {
     var p = Array(repeating: 0, count: A.count + 1)
     var result = 0
-    
+
     for i in 1..<A.count + 1 {
         p[i] = p[i - 1] + A[i - 1]
     }
-    
+
     for i in 0..<A.count {
         if A[i] == 0 {
             result += p[p.count - 1] - p[i + 1]
         }
-        
-        if result > 1_000_000_000 {
-            return -1
-        }
+
+        guard result <= 1_000_000_000 else { return -1 }
     }
-    
+
     return result
 }
 
