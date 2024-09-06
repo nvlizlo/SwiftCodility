5d983a12e52c06f7e03924922227056375deaaef
Nazariy Vlizlo <nazariy.vlizlo@openbet.com>
2024-09-06 11:29:49 +0300

    Updated solution to PrefixSums/CountDiv
    gitIgnore

--- Lesson 5 - Prefix Sums/CountDiv.playground/Contents.swift
+++ Lesson 5 - Prefix Sums/CountDiv.playground/Contents.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 public func solution(_ A : Int, _ B : Int, _ K : Int) -> Int {
-    return B / K - A / K + (A % K == 0 ? 1 : 0)
+    (B - A) / K + (A % K == 0 ? 1 : 0)
 }
 
 solution(7, 12, 2)
