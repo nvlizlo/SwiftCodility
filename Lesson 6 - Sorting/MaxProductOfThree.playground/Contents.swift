87ed24e4724d9c2d1bff233024cfda05ef51ab14
Nazariy Vlizlo <nazariy.vlizlo@openbet.com>
2024-09-09 20:00:24 +0300

    Updated solution for Sorting/MaxProductOfThree
    And gitIgnore

--- Lesson 6 - Sorting/MaxProductOfThree.playground/Contents.swift
+++ Lesson 6 - Sorting/MaxProductOfThree.playground/Contents.swift
@@ -5,9 +5,9 @@ import UIKit
 public func solution(_ A : inout [Int]) -> Int {
     A.sort()
     let count = A.count
-    let max1 = A[count-1]*A[count-2]*A[count-3]
-    let max2 = A[count-1]*A[0]*A[1]
-    
+    let max1 = A[count - 1] * A[count - 2] * A[count - 3]
+    let max2 = A[count - 1] * A[0] * A[1]
+
     return max(max1, max2)
 }
 
