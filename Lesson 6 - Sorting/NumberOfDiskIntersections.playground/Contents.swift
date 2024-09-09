180226cde9019a87af2ec6dc56a3fe0a7122b5f9
Nazariy Vlizlo <nazariy.vlizlo@openbet.com>
2024-09-09 21:55:59 +0300

    Updated solution for Sorting/NumberOfDiscIntersections

--- Lesson 6 - Sorting/NumberOfDiskIntersections.playground/Contents.swift
+++ Lesson 6 - Sorting/NumberOfDiskIntersections.playground/Contents.swift
@@ -2,34 +2,30 @@ import UIKit
 
 public func solution(_ A : inout [Int]) -> Int {
     
-    if A.isEmpty || A.count > 100_000 {
-        return 0
-    }
-    
+    guard !A.isEmpty, A.count <= 100_000 else { return 0 }
+
     let maxValue = A.max() ?? 0
     let sortedArray = A.enumerated().map { ($0.offset - $0.element, $0.offset + $0.element) }.sorted { $0.0 < $1.0 }
-    
+
     var intersections = 0
     
     for i in 0..<A.count {
-        
+
         let iRight = sortedArray[i].1
         let maxIndex = min(iRight + maxValue + 1, A.count)
         
-        for j in i+1..<maxIndex {
-            
+        for j in i + 1..<maxIndex {
+
             let jLeft = sortedArray[j].0
             
             if iRight >= jLeft {
                 intersections += 1
             }
         }
-        
-        if intersections > 10_000_000 {
-            return -1
-        }
+
+        guard intersections <= 10_000_000 else { return -1 }
     }
-    
+
     return intersections
 }
 
