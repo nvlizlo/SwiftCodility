884c8a5d97ba30484afbba74272b97ce5bed3e6a
Nazariy Vlizlo <nazariy.vlizlo@openbet.com>
2024-09-09 19:56:00 +0300

    Refactored solution for Sorting/Distinct
    And gitignore

--- Lesson 6 - Sorting/Distinct.playground/Contents.swift
+++ Lesson 6 - Sorting/Distinct.playground/Contents.swift
@@ -3,13 +3,13 @@
 import UIKit
 
 public func solution(_ A : inout [Int]) -> Int {
-    if A.isEmpty {
-        return 0
-    }
+    guard !A.isEmpty else { return 0 }
+
     A.sort()
     var result = 1
+
     for i in 1..<A.count {
-        if A[i] != A[i-1] {
+        if A[i] != A[i - 1] {
             result += 1
         }
     }
